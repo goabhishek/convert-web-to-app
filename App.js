e2f6c883a016b3001124edc58d62aead90605954
goabhishek <mabhishek4156@gmail.com>
2022-11-11 17:42:17 +0530

    add link

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello</Text>
+      <WebView source={{ uri: 'https://reactnative.dev' }} />
     </View>
   );
 }
